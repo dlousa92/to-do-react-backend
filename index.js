bbf3efff36f18cfea0007e708c71b42772303eae
Daniel Lousa <dlousa92@gmail.com>
2018-09-05 16:16:21 -0400

    create basic get request that will return list of all to-do items

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const express = require('express')
 const parser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
-
+const Item = mongoose.model('Item')
 const app = express()
 
-app.listen(process.env.port || 8000)
+app.use(parser.json())
+app.use(cors())
+
+app.get('/api/todo', (req, res) => {
+  Item.find()
+    .then((items) => {
+      res.json(items)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+app.listen(process.env.PORT || 8000)
