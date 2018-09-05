6edb6f093942f275e60f714bae3de7237a590ddd
Daniel Lousa <dlousa92@gmail.com>
2018-09-05 16:47:59 -0400

    have to register model Item, keep running into bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const parser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const Item = mongoose.model('Item')
+
 const app = express()
 
 app.use(parser.json())
